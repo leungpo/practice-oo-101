db433e359f2d2c3ca1081fc600ce88af7f15e4c0
leungpo <polly.leung@oocl.com>
2022-11-30 16:44:20 +0800

    feat: add new method for Car

--- src/main/java/com/oo101/Car.java
+++ src/main/java/com/oo101/Car.java
@@ -5,4 +5,7 @@ public class Car extends Vehicle{
     public Car(String name, int speed) {
         super(name, speed);
     }
+    public Car(String name, GasolineEngine gasolineEngine) {
+        super(name, gasolineEngine.getSpeed());
+    }
 }
