6590439a19f728eb788ddf97a2fce4b851c4dc86
leungpo <polly.leung@oocl.com>
2022-11-30 16:14:59 +0800

    feat: add vehicle class

---
+++ src/main/java/com/oo101/Vehicle.java
@@ -0,0 +1,14 @@
+package com.oo101;
+
+public class Vehicle {
+    private final String name;
+    private final int speed;
+
+    public Vehicle(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+    public String speedUp(){
+        return name + ": speed up " + speed + " km/h";
+    }
+}
